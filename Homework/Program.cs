3298806f95f26b6230e5ebefd6cb8ba1a7c919ce
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-03-22 17:15:10 +0300

    added code solution

---
+++ Homework/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+string[] array1 = new string[5] { "Hi", "world", "199", "Ops", "final" };
+string[] array2 = new string[array1.Length];
+
+void SortArray(string[] array1, string[] array2)
+{
+    int count = 0;
+
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array1);
+SortArray(array1, array2);
+PrintArray(array2);
